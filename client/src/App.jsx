3038a966034c64e54573f56b14ddee6e288408fc
WesPalanca <wespalanca@gmail.com>
2025-07-14 11:08:41 -0400

    added new pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import Auth from './pages/Auth';
+import ProtectedRoute from './components/ProtectedRoute';
+import Dashboard from './pages/Dashboard';
 
 const App = () => {
   return (
-    <div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/auth' element={<Auth />}/>
+        <Route path='/dashboard' element = {<ProtectedRoute> <Dashboard></Dashboard> </ProtectedRoute>} />
 
-    </div>
+        
+      </Routes>
+    </Router>
   )
 }
+
+
+export default App;
