082f51d75ec9cb4b3693ab46e58b6916eec05d5c
WesPalanca <wespalanca@gmail.com>
2025-07-19 15:10:49 -0400

    added ui component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
+import { ThemeProvider } from './components/ThemeContext.jsx'
 import './App.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
