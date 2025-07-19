a10580fbafa10c3bb6f54a83e6b0600eb2a262b5
WesPalanca <wespalanca@gmail.com>
2025-07-19 15:11:01 -0400

    added new pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import LandingPage from './pages/LandingPage';
 import Auth from './pages/Auth';
 import ProtectedRoute from './components/ProtectedRoute';
 import Dashboard from './pages/Dashboard';
+import UserLogs from './pages/UserLogs';
+import CreateLog from './pages/CreateLog';
+import SingleLogPage from './pages/SingleLogPage';
 
 const App = () => {
   return (
@@ -10,7 +13,10 @@ const App = () => {
       <Routes>
         <Route path='/' element={<LandingPage />} />
         <Route path='/auth' element={<Auth />}/>
-        <Route path='/dashboard' element = {<ProtectedRoute> <Dashboard></Dashboard> </ProtectedRoute>} />
+        <Route path='/dashboard' element = {<ProtectedRoute> <Dashboard /> </ProtectedRoute>} />
+        <Route path='/logs' element={<ProtectedRoute> <UserLogs /> </ProtectedRoute>}></Route>
+        <Route path='/logs/create' element={<ProtectedRoute> <CreateLog /> </ProtectedRoute>} />
+        <Route path="/logs/:logId" element={<ProtectedRoute> <SingleLogPage /> </ProtectedRoute>} />
 
         
       </Routes>
