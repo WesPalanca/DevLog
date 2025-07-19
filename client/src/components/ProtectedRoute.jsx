890b791267623c280dfdcf80c82315bcf4cfdd7e
WesPalanca <wespalanca@gmail.com>
2025-07-19 15:05:10 -0400

    fixed navigation route

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,14 +1,27 @@
 import { Navigate } from "react-router-dom";
+import { jwtDecode } from 'jwt-decode'
 
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token");
 
-
   if (!token) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/Auth" replace />;
   }
 
+  try {
+    const { exp } = jwtDecode(token);
+    if (!exp || Date.now() >= exp * 1000) {
+      // Token expired
+      localStorage.removeItem("token");
+      return <Navigate to="/Auth" replace />;
+    }
+  } catch (error) {
+    // Invalid token format or decode error
+    localStorage.removeItem("token");
+    return <Navigate to="/Auth" replace />;
+  }
 
+  // Token valid
   return children;
 };
 
