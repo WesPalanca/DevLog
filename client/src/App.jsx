8656ce5c313042fbe3d0682fea4939f4d01bde50
WesPalanca <wespalanca@gmail.com>
2025-07-22 19:09:17 -0400

    added new pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Dashboard from './pages/Dashboard';
 import UserLogs from './pages/UserLogs';
 import CreateLog from './pages/CreateLog';
 import SingleLogPage from './pages/SingleLogPage';
+import Profile from './pages/Profile';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route path='/logs' element={<ProtectedRoute> <UserLogs /> </ProtectedRoute>}></Route>
         <Route path='/logs/create' element={<ProtectedRoute> <CreateLog /> </ProtectedRoute>} />
         <Route path="/logs/:logId" element={<ProtectedRoute> <SingleLogPage /> </ProtectedRoute>} />
+        <Route path="/profile" element={<ProtectedRoute> <Profile /> </ProtectedRoute>} />
 
         
       </Routes>
