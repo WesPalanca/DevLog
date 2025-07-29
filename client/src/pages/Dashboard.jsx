8ac61a526eaaa4d247bfd38e5a9af22906c58299
WesPalanca <wespalanca@gmail.com>
2025-07-29 09:36:04 -0400

    fixed url

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -10,7 +10,7 @@ const Dashboard = () => {
   const { nightMode, setNightMode} = useTheme();
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [username, setUsername] = useState("[John Doe]");
-  const apiUrl = import.meta.env.VITE_DEV_URL;
+  const apiUrl = import.meta.env.VITE_API_URL;
 
   useEffect(() => {
     const token = localStorage.getItem("token");
